2da99f4e4bf74bc300066d160721c59168590ca9
dipankar-halder <dipankar15-11137@diu.edu.bd>
2024-05-16 23:33:59 +0600

    add appointment

--- quires-client/src/Components/Pages/Home/Home.js
+++ quires-client/src/Components/Pages/Home/Home.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { BsFillQuestionSquareFill } from 'react-icons/bs';
 import { useLocation } from 'react-router-dom';
 import AddPost from './AddPost/AddPost';
 import HomeSections from './Home Section/HomeSections';
+import InquireFrom from './Home Section/InquireFrom/InquireFrom';
 import LeftSide from './Home Section/LeftSide/LeftSide';
 import RightSide from './Home Section/RightSide/RightSide';
 
+
 const Home = ({ searchGet }) => {
+   const [opinion, setOpinion] = useState(false);
   const { pathname } = useLocation();
 
   useEffect(() => {
@@ -17,6 +21,7 @@ const Home = ({ searchGet }) => {
   }
   return (
     <div className="pt-[66px] text-white">
+      
       <div className="grid grid-cols-12">
         <div className="col-span-2 border-r-[1px] border-slate-700 ">
           <div className="fixed mt-5 ml-5">
@@ -32,6 +37,24 @@ const Home = ({ searchGet }) => {
             <RightSide />
           </div>
         </div>
+      </div>
+
+      <div className="fixed z-50 right-5 bottom-10 ">
+        {opinion && (
+          <div className="bg-green-600 mr-7 p-3 rounded-t-lg rounded-l-lg ">
+            <InquireFrom setOpinion={setOpinion} />
+          </div>
+        )}
+        <h1 className=" text-4xl text-green-500 flex justify-end cursor-pointer">
+          {opinion ? (
+            <BsFillQuestionSquareFill
+              className="rotate-180 text-red-600 "
+              onClick={() => setOpinion(false)}
+            />
+          ) : (
+            <BsFillQuestionSquareFill onClick={() => setOpinion(true)} />
+          )}
+        </h1>
       </div>
     </div>
   );
